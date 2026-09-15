230ac9fc3c425c28cdd3f0ca6e6f1e9dca10becc
Dhruv Patel <dhruv.patel0839@gmail.com>
2026-09-15 11:57:46 -0400

    Implemented prompting for part1

--- src/lexer.c
+++ src/lexer.c
@@ -6,7 +6,7 @@
 int main()
 {
 	while (1) {
-		printf("> ");
+		print_prompt();
 
 		/* input contains the whole command
 		 * tokens contains substrings from input split by spaces
@@ -88,4 +88,33 @@ void free_tokens(tokenlist *tokens) {
 		free(tokens->items[i]);
 	free(tokens->items);
 	free(tokens);
+}
+
+// Print prompt using env variables
+void print_prompt(void)
+{
+    char* user = getenv("USER");
+    char* machine = getenv("MACHINE");
+    char* pwd = getenv("PWD");
+
+    // If NULL -> make unknown for now
+    if (user == NULL)
+    {
+        user = getenv("LOGNAME");
+        if (user == NULL)
+        {
+            user = "unknown";
+        }
+    }
+    if (machine == NULL)
+    {
+        machine = "unknown";
+    }
+    if (pwd == NULL)
+    {
+        pwd = "unknown";
+    }
+
+    printf("%s@%s:%s>", user, machine, pwd);
+    fflush(stdout);
 }
