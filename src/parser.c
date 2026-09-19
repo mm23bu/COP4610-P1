2213c677d67e6d5261e5e151f1adb058c4f14b75
rodneycd <codingwithrod@gmail.com>
2026-09-19 12:47:08 -0400

    implement tilde expansion

--- src/parser.c
+++ src/parser.c
@@ -8,6 +8,20 @@ void expand_tokens(tokenlist *tokens) {
     for (size_t i = 0; i < tokens->size; i++) {
         char *item = tokens->items[i];
 
+        //check for ~ or ~/ pattern
+        if (item[0] == '~' && (item[1] == '\0' || item[1] == '/')){
+            char *home = getenv("HOME");
+            if (home){
+                //allocate space for string to include home + path
+                char *tilde_expanded = malloc(strlen(home) + strlen(item));
+                strcpy(tilde_expanded, home);
+                strcat(tilde_expanded, item+1);
+                free(tokens->items[i]);
+                tokens->items[i] = tilde_expanded;
+                item = tilde_expanded;
+            }
+        }
+
         /* env variables */
         if (item[0] == '$' && strlen(item) > 1) {
             char *var_name = item + 1;
