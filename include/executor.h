12a58cff2059aaa0cc023599bfe295c28a9512b8
mm23bu <mm23bu@fsu.edu>
2026-09-21 01:00:53 -0400

    command execution header starting

--- include/executor.h
+++ include/executor.h
@@ -1,1 +1,10 @@
-/* parts 4, 5, 6, 7 */
+/* parts 4, 5, 6, 7 headers*/
+
+#infdef EXECUTOR_H
+#define EXECUTOR_H
+
+#include "shell.h"
+
+void execute_pipeline(tokenlist *tokens, const char *raw_cmd, char *raw_input); // raw_cmd = original command string, raw_input = buffer from get_input
+
+#endif
