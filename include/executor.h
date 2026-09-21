69c5a120bbc641618f757c7328a892f453b58194
mm23bu <mm23bu@fsu.edu>
2026-09-21 13:51:14 -0400

    executor initialization

--- include/executor.h
+++ include/executor.h
@@ -1,10 +1,10 @@
 /* parts 4, 5, 6, 7 headers*/
 
-#infdef EXECUTOR_H
+#ifndef EXECUTOR_H
 #define EXECUTOR_H
 
 #include "shell.h"
 
-void execute_pipeline(tokenlist *tokens, const char *raw_cmd, char *raw_input); // raw_cmd = original command string, raw_input = buffer from get_input
+void execute_pipeline(tokenlist *tokens, const char *raw_cmd); // raw_cmd = original command string
 
 #endif
--- src/executor.c
+++ src/executor.c
@@ -1,1 +1,28 @@
-// parts 4-7 imp 
+// parts 4-7 imp
+
+#include "executor.h"
+
+
+void execute_pipeline(tokenlist *tokens, const char *raw_cmd){
+    if (!tokens || tokens->size == 0) {
+      return;
+    } 
+
+    size_t count = tokens->size;
+
+    int is_bg = 0;
+
+    // check for trailing '&', which leads to background process
+    if (count > 0 && strcmp(tokens->items[count - 1], "&") == 0){
+      is_bg = 1;
+      free(tokens->items[count-1]);
+      tokens->items[count - 1] = NULL;
+      tokens->size--;
+      count--;
+    }
+
+    if (count == 0){
+      return;
+    }
+    
+}
