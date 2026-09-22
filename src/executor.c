4ac847271b489578f0e4bd90825db51668879df2
mm23bu <mm23bu@fsu.edu>
2026-09-22 17:19:33 -0400

    slicing commands and extracting redirections

--- src/executor.c
+++ src/executor.c
@@ -18,7 +18,7 @@ static void run_child(char **argv, int in_fd, int out_fd) {
       close(out_fd);
     }
 
-    // execv from here
+    /* execve from here */
 }
 
 void execute_pipeline(tokenlist *tokens, const char *raw_cmd){
@@ -43,12 +43,60 @@ void execute_pipeline(tokenlist *tokens, const char *raw_cmd){
       return;
     }    
 
-    //dummy call 
-    if (0) {
-        run_child(tokens->items, STDIN_FILENO, STDOUT_FILENO);
-    }
-    (void)is_bg;
+    /* slice commands at pipes and extract redirections */
+    char **cmd_argv[MAX_COMMANDS];
+    char *input_file = NULL;
+    char *output_file = NULL;
+    int cmd_count = 0;
 
-    /* redirections and piping from here */
+    cmd_argv[cmd_count++] = &tokens->items[0];
+
+    for (size_t i=0; i< tokens->size; i++){
+        if (!tokens->items[i]){
+          continue;
+        }  
+
+        if (strcmp(tokens->items[i], "|") == 0) {
+          free(tokens->items[i]);
+          tokens->items[i] = NULL;
+          if (cmd_count >= MAX_COMMANDS){
+            fprintf(stderr, "Error: maximum 2 pipes allowed\n");
+            return;
+          }
+
+          if (i+1 < tokens->size) {
+            cmd_argv[cmd_count++] = &tokens->items[i+1];    
+          }
+        }
+
+        else if (strcmp(tokens->items[i], "<") == 0){
+          if (cmd_count > 1) {
+            fprintf(stderr, "Error: input redirection only allowed on first command\n");
+            return;
+          }
+          free(tokens -> items[i]);
+          tokens->items[i] = NULL;
+          if (i+1 < tokens->size){
+            input_file = tokens->items[++i];
+          }          
+        }
+
+        else if (strcmp(tokens->items[i], ">") == 0){
+          free(tokens->items[i]);
+          tokens->items[i] = NULL;
+          if (i+1 < tokens->size) {
+            output_file = tokens->items[++i];
+          }
+        }
+    }
+
+    /* suppress unused warnings until all functionalities are implemented */
+    (void)is_bg;
+    (void)input_file;
+    (void)output_file;
+    (void)cmd_argv;
+    (void)raw_cmd;
+    (void)run_child;
+  /* verification, forking, and running pipleline stages */
 
 }
